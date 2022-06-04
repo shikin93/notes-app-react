ae8ccde0f45ac617b2e52fb3ba9374c4fd4f7512
shikin93 <official.shiin@gmail.com>
2022-06-05 01:46:36 +0700

    setup packageeslint, tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'mobile': '375px',
+      'tablet': '768px',
+      'laptop': '981px',
+      'desktop': '1440px',
+    },
+  },
+  plugins: [
+  ],
+}
